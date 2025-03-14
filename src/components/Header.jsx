cbc157ea6444dd9a17e4397cbbbdd03c119224a7
oskaritauru <oskari.tauru@hotmail.fi>
2025-03-14 13:07:28 +0200

    second commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import chefClaudeLogo from "../images/icons8-chef.png";
+
+export default function Header() {
+  return (
+    <header>
+      <img src={chefClaudeLogo} />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+}
